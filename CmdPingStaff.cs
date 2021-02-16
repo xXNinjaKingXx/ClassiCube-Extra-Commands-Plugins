a3e7769e51d1b567383695a2ccf8ea6b8c30d224
xXNinjaKingXx <79152040+xXNinjaKingXx@users.noreply.github.com>
2021-02-16 12:08:18 -0500

    Update CmdPingStaff.cs

--- CmdPingStaff.cs
+++ CmdPingStaff.cs
@@ -1,31 +1,34 @@
-// This Command Was Made By: Ninja_King
-// Please Edit Lines 20,21,22 
+// This Command Was Made By: Ninja_King. Helped by SpicyCombo
+// Please Edit Lines 17, 18, 19, 20, 21, and 22
 // Notice: You will Need to add the discrodbroadcast Cmd before being able to use this Command
 
 using System;
+using MCGalaxy;
 
-namespace MCGalaxy 
-{
-	public class CmdPingStaff : Command2
-	{
-		
-		public override string name { get { return "PingStaff"; } }
-		public override string shortcut { get { return "png"; } }
-		public override bool MessageBlockRestricted { get { return false; } }
-		public override string type { get { return "other"; } }
-		public override bool museumUsable { get { return false; } }
-		public override LevelPermission defaultRank { get { return LevelPermission.Operator; } }
-		public override void Use(Player p, string message, CommandData data)
-		{
-        	// Configure Ping Names below.
-            Command.Find("discordbroadcast").Use(p, "<Discord Id here");
-            Command.Find("discordbroadcast").Use(p, "<Discord Id here>");
-            Command.Find("discordbroadcast").Use(p, "<Discord Id here>");
-		}
-		public override void Help(Player p)
-		{
-			p.Message("/PingStaff");
-            p.Message("%HPings Staff Members Ranked Administrator+");
-		}
-	}
+namespace MCGalaxy.Commands.CPE {
+    public sealed class CmdDiscordPing : Command2 {
+        public override string name { get { return "PingStaff"; } }
+        public override string shortcut { get { return "png"; } }
+        public override string type { get { return "other"; } }
+
+        public override void Use(Player p, string message)
+        {
+            string[] args = message.SplitSpaces(2);
+            if (args[0] == "Ingame Username 1")
+            { Command.Find("DiscordBroadcast").Use(p, "Discord User ID 1"); }
+            else if (args[0] == "Ingame Username 2")
+            { Command.Find("DiscordBroadcast").Use(p, "Discord User ID 2"); }
+            else if (args[0] == "Ingame Username 3")
+            { Command.Find("DiscordBroadcast").Use(p, "<Discord User ID 3"); }
+
+        }
+                
+                    
+    public override void Help(Player p)
+        {
+            p.Message("%T/Ping [Staff Member] <Message>");
+            p.Message("%HPings [Staff Member] on Discord");
+            p.Message("%4It's case sensitive! &4Use If Necessary");
+        }
+    }
 }
