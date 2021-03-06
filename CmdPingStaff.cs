a9afd019be83329038035c1d90a7173f608907da
xXNinjaKingXx <79152040+xXNinjaKingXx@users.noreply.github.com>
2021-03-06 13:24:54 -0500

    Updated

--- CmdPingStaff.cs
+++ CmdPingStaff.cs
@@ -1,6 +1,16 @@
 // This Command Was Made By: Ninja_King. Helped by SpicyCombo
-// Please Edit Lines 17, 18, 19, 20, 21, and 22
+// Please Edit Lines 24,25,26, 27, 28, and 29. Change all Ingame Username To [Ingame Staff Username]
 // Notice: You will Need to add the discrodbroadcast Cmd before being able to use this Command
+// █▀▀ █▀█ █▀▀ ▄▀█ ▀█▀ █▀█ █▀█ ▀
+// █▄▄ █▀▄ ██▄ █▀█ ░█░ █▄█ █▀▄ ▄
+
+
+// ███╗░░██╗██╗███╗░░██╗░░░░░██╗░█████╗║
+// ████╗░██║██║████╗░██║░░░░░██║██╔══██║
+// ██╔██╗██║██║██╔██╗██║░░░░░██║███████║
+// ██║╚████║██║██║╚████║██╗░░██║██╔══██║
+// ██║░╚███║██║██║░╚███║╚█████╔╝██║░░██║
+
 
 using System;
 using MCGalaxy;
@@ -19,7 +29,7 @@ namespace MCGalaxy.Commands.CPE {
             else if (args[0] == "Ingame Username 2")
             { Command.Find("DiscordBroadcast").Use(p, "Discord User ID 2"); }
             else if (args[0] == "Ingame Username 3")
-            { Command.Find("DiscordBroadcast").Use(p, "<Discord User ID 3"); }
+            { Command.Find("DiscordBroadcast").Use(p, "Discord User ID 3"); }
 
         }
                 
@@ -28,7 +38,7 @@ namespace MCGalaxy.Commands.CPE {
         {
             p.Message("%T/Ping [Staff Member] <Message>");
             p.Message("%HPings [Staff Member] on Discord");
-            p.Message("%4It's case sensitive! &4Use If Necessary");
+            p.Message("%4It's case sensitive! &4Use If Necessary!");
         }
     }
 }
