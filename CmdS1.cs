83263d80545449122f64175058d91f252c3cd758
xXNinjaKingXx <79152040+xXNinjaKingXx@users.noreply.github.com>
2021-10-05 10:43:25 -0400

    Add files via upload

---
+++ CmdS1.cs
@@ -0,0 +1,43 @@
+// █▀▀ █▀█ █▀▀ ▄▀█ ▀█▀ █▀█ █▀█ ▀
+// █▄▄ █▀▄ ██▄ █▀█ ░█░ █▄█ █▀▄ ▄
+
+
+// ███╗░░██╗██╗███╗░░██╗░░░░░██╗░█████╗║
+// ████╗░██║██║████╗░██║░░░░░██║██╔══██║
+// ██╔██╗██║██║██╔██╗██║░░░░░██║███████║
+// ██║╚████║██║██║╚████║██╗░░██║██╔══██║
+// ██║░╚███║██║██║░╚███║╚█████╔╝██║░░██║
+
+using System;
+using System.IO; // Allows us to use File
+
+using MCGalaxy;
+
+namespace MCGalaxy
+{
+    public sealed class CmdAdmins : Command2
+    {
+        public override string name { get { return "S1"; } }
+        public override string type { get { return "other"; } }
+        public override LevelPermission defaultRank { get { return LevelPermission.Guest; } }
+
+        public override void Use(Player p, string message, CommandData data)
+        {
+            string path = @"./text/Leaders.txt";
+
+            // Check to see if file exists
+            if (File.Exists(path))
+            {
+                string[] lines = File.ReadAllLines(path); // Grab lines of file
+                string leaders = string.Join(", ", lines); // Use .Join(", ") so output looks like this: admin1, admin2, admin3
+
+                p.Message(leaders); // Send to player
+            }
+        }
+
+        public override void Help(Player p)
+        {
+
+        }
+    }
+}
