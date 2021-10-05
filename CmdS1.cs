75558ce311bdbc9b82074dd38f29e4c90b5ca27e
xXNinjaKingXx <79152040+xXNinjaKingXx@users.noreply.github.com>
2021-10-05 10:59:16 -0400

    Update CmdS1.cs

--- CmdS1.cs
+++ CmdS1.cs
@@ -1,3 +1,8 @@
+//Notice: You will need to edit lines 31, 37, and 39. Replace all [Putranknamehere] to the rank you want it to show. E.g, [Putranknamehere] = Administrator.
+//Notice: This will only one rank.
+//Notice: You will need to create a file in the text folder named "[Putranknamehere].txt
+//Notice: You will have to add the names of the players with [putranknamehere] to the new .txt file you have created.
+
 // █▀▀ █▀█ █▀▀ ▄▀█ ▀█▀ █▀█ █▀█ ▀
 // █▄▄ █▀▄ ██▄ █▀█ ░█░ █▄█ █▀▄ ▄
 
@@ -15,7 +20,7 @@ using MCGalaxy;
 
 namespace MCGalaxy
 {
-    public sealed class CmdAdmins : Command2
+    public sealed class CmAdmins : Command2
     {
         public override string name { get { return "S1"; } }
         public override string type { get { return "other"; } }
@@ -23,15 +28,15 @@ namespace MCGalaxy
 
         public override void Use(Player p, string message, CommandData data)
         {
-            string path = @"./text/Leaders.txt";
+            string path = @"./text/[Putranknamehere].txt";
 
             // Check to see if file exists
             if (File.Exists(path))
             {
                 string[] lines = File.ReadAllLines(path); // Grab lines of file
-                string leaders = string.Join(", ", lines); // Use .Join(", ") so output looks like this: admin1, admin2, admin3
+                string [Putranknamehere] = string.Join(", ", lines); // Use .Join(", ") so output looks like this: admin1, admin2, admin3
 
-                p.Message(leaders); // Send to player
+                p.Message(Putranknamehere); // Send to player
             }
         }
 
